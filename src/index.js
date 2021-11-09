89b70dda9fe65bd127b6e546ca5a4be94127b8d4
Diego Moraes <diegomoraes.a@gmail.com>
2021-11-08 22:26:40 -0300

    splash screen in progress

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Splash from './splash/Splash';
 import reportWebVitals from './reportWebVitals';
+import './fonts/NunitoSans-Regular.ttf'; 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Splash />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/splash/Splash.js
@@ -0,0 +1,41 @@
+import "./Splash.css";
+
+function Splash() {
+  return (
+    <div class="screen_background">
+      <div class="logo_background">
+        <div class="logo_background1" />
+      </div>
+      <div class="top_left_square" />
+      <div class="logo" />
+      <div class="bottom_left_weed" />
+      <div class="bottom_right_weed" />
+      <span class="version_label">
+        Version <span class="text_green">2.0</span> - Launching&nbsp;
+        <span class="text_green">02/2022</span>
+      </span>
+      <div class="logo_name" />
+      <div class="text_input">
+        <div class="text_input1" />
+        <span class="text_input2">Notify me, I want to weed smarter!</span>
+        <span class="text_input3">EMAIL ADDRESS</span>
+        <div class="text_input4" />
+      </div>
+      <span class="copyrights">
+        © 2021 - 2022 Terpli, Inc. All Rights Reserved.
+      </span>
+      <div class="checkbox_input">
+        <span class="checkbox_input1">
+          Sign me up to Beta Test the new App (Free)
+        </span>
+        <div class="checkbox_input2" />
+      </div>
+      <span class="description">
+        Personalizing Your Cannabis Experience Using Reviews, Cannabinoids &
+        Terpene Data!
+      </span>
+    </div>
+  );
+}
+
+export default Splash;
