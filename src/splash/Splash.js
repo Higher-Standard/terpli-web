705b28b8b3cb35f0ffd81989252767823cbe9cb9
Diego Moraes <diegomoraes.a@gmail.com>
2021-11-09 22:17:17 -0300

    fix css and add inputs

--- src/splash/Splash.js
+++ src/splash/Splash.js
@@ -2,53 +2,52 @@ import "./Splash.scss";
 
 function Splash() {
   return (
-    <div class="container">
-      <div class="background_images">
-        <div class="top_left_square" />
-        <div class="bottom_left_weed" />
-        <div class="bottom_right_weed" />
+    <div className="container">
+      <div className="background_images">
+        <div className="top_left_square" />
+        <div className="bottom_left_weed" />
+        <div className="bottom_right_weed" />
       </div>
-      <div class="content">
-        <div class="content_logo">
-          <div class="logo_name" />
-          <div class="logo_background">
-            <div class="logo_background1" />
+      <div className="content">
+        <div className="content_logo">
+          <div className="logo_name" />
+          <div className="logo_background">
+            <div className="logo_background1" />
           </div>
-          <div class="logo" />
+          <div className="logo" />
         </div>
-        <div class="content_text">
-          <span class="version_label">
-            Version <span class="text_green">2.0</span> - Launching&nbsp;
-            <span class="text_green">02/2022</span>
+        <div className="content_text">
+          <span className="version_label">
+            Version <span className="text_green">2.0</span> - Launching&nbsp;
+            <span className="text_green">02/2022</span>
           </span>
-          <span class="description">
+          <span className="description">
             Personalizing Your Cannabis Experience Using Reviews, Cannabinoids
             &amp; Terpene Data!
           </span>
         </div>
-        <div class="content_input">
-          <div class="text_input">
-            <div class="text_input1" />
-            <span class="text_input2">
+        <div className="content_input">
+          <form>
+            <div className="text_input">
+              <div className="text_input1" />
               <input
+                className="text_input2"
                 type="email"
-                id="email"
                 placeholder="Notify me, I want to weed smarter!"
-                required
               />
-            </span>
-            <span class="text_input3">EMAIL ADDRESS</span>
-            <div class="text_input4" />
-          </div>
-          <div class="checkbox_input">
-            <span class="checkbox_input1">
-              Sign me up to Beta Test the new App (Free)
-            </span>
-            <div class="checkbox_input2" />
-          </div>
+              <span className="text_input3">EMAIL ADDRESS</span>
+              <div className="text_input4" />
+            </div>
+            <div className="checkbox_input">
+              <span className="checkbox_input1">
+                Sign me up to Beta Test the new App (Free)
+              </span>
+              <input className="checkbox_input2" type="checkbox" />
+            </div>
+          </form>
         </div>
-        <div class="content_footer">
-          <span class="copyrights">
+        <div className="content_footer">
+          <span className="copyrights">
             &copy; 2021 - 2022 Terpli, Inc. All Rights Reserved.
           </span>
         </div>
